e5adafff8f01564c8126113c671e18fbe694a04b
Sylvester Keil <sylvester@keil.or.at>
2016-08-25 14:09:29 +0200

    Use njk instead of ejs

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var nunjucks = require('nunjucks');
 
 var routes = require('./routes/index');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
+
+app.set('view engine', 'njk');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
