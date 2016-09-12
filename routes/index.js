5a5761d84fbb541bcc3158bdac327e9c52d05bea
Sylvester Keil <sylvester@keil.or.at>
2016-09-12 16:48:29 +0200

    Add terms-of-service
    Mark active links

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,19 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Tropy' })
+  res.render('index', {
+    path: req.path,
+    title: 'Tropy'
+  })
 })
+
+router.get('/terms-of-service', (req, res) => {
+  res.render('terms-of-service', {
+    path: req.path,
+    layout: 'index.njk',
+    title: 'Tropy · Terms of Service'
+  })
+})
+
 
 module.exports = router
