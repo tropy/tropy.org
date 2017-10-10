3162c3c6d3b3b67bb6214fe1f211e903e04dc565
Sylvester Keil <sylvester@keil.or.at>
2017-10-10 14:30:24 +0200

    Add license and credits pages
    See #6

--- routes/index.js
+++ routes/index.js
@@ -17,5 +17,19 @@ router.get('/terms-of-service', (req, res) => {
   })
 })
 
+router.get('/credits', (req, res) => {
+  res.render('credits', {
+    path: req.path,
+    title: 'Tropy · Credits'
+  })
+})
+
+router.get('/license', (req, res) => {
+  res.render('license', {
+    path: req.path,
+    title: 'Tropy · License'
+  })
+})
+
 
 module.exports = router
