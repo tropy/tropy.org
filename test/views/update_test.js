bf50e3109e0187d8b298695f0e6627c0a4e0292f
Sylvester Keil <sylvester@keil.or.at>
2017-10-18 22:38:42 +0200

    Add update response test

--- test/views/update_test.js
+++ test/views/update_test.js
@@ -9,5 +9,21 @@ describe('GET /update/(:channel/):platform/:version', () => {
       .get(`/update/beta/darwin/${versions.beta[0]}`)
       .then(res => { expect(res).to.have.status(204) }))
 
-})
+  describe('when an update is available', () => {
+    beforeEach(() =>
+      void versions.beta.unshift('LATEST'))
+    afterEach(() =>
+      void versions.beta.shift())
 
+    it('returns an update resposnse', () =>
+      request(app)
+        .get(`/update/beta/darwin/${versions.beta[1]}`)
+        .then(res => {
+          expect(res).to.have.status(200)
+          expect(res.body).to.eql({
+            url: 'https://github.com/tropy/tropy/releases/download/LATEST/tropy-LATEST.dmg',
+            name: 'LATEST'
+          })
+        }))
+  })
+})
