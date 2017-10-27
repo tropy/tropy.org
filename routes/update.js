98b5987672e2442e4cabbda2cf8449f90efbd5af
Sylvester Keil <sylvester@keil.or.at>
2017-10-27 22:47:11 +0200

    Improve auto-update routes
    Manage windows releases on server

--- routes/update.js
+++ routes/update.js
@@ -2,45 +2,89 @@
 
 const express = require('express')
 const api = module.exports = express.Router()
-const versions = require('../versions')
 const download = require('./download')
+
+const VERSIONS = require('../versions')
+const RELEASES = require('../releases')
+
+RELEASES.x64.stable = RELEASES.x64.stable.join('\r\n')
+RELEASES.x64.beta = RELEASES.x64.beta.join('\r\n')
+RELEASES.x32.stable = RELEASES.x32.stable.join('\r\n')
+RELEASES.x32.beta = RELEASES.x32.beta.join('\r\n')
+
 
 const variants = {
   darwin: 'zip', linux: '', win32: ''
 }
 
-const CHANNEL = '/:channel(dev|beta|stable)'
-const PLATFORM = '/:platform(darwin|linux|win32)'
+const CHANNEL = '(/:channel(dev|beta|stable))'
+const ARCH = '(/:arch(x32|x64))?'
+const PLATFORM = '(/:platform(darwin|linux))'
 
-api.get(`${CHANNEL}${PLATFORM}(/:version)(/:file)?`, (req, res) => {
+
+// Linux and macOS
+api.get(`${CHANNEL}${PLATFORM}${ARCH}/:version`, (req, res) => {
   const channel = req.params.channel || 'stable'
   const arch = req.params.arch || 'x64'
-  const platform = download.getPlatform(req)
+  const platform = req.params.platform
   const version = req.params.version
   const variant = variants[platform]
-  const file = req.params.file
 
-  if (file != null && platform === 'win32') {
-    const url = download.getAssetFolder(versions[channel][0])
-
-    if (url && (isReleases(file) || isPkg(file))) {
-      return res.redirect(`${url}/${file}`)
-    }
-
-    res.status(404).send('Not Found')
-
-  } else if (versions[channel].indexOf(version) > 0) {
-    const name = versions[channel][0]
+  if (VERSIONS[channel].indexOf(version) > 0) {
+    const name = VERSIONS[channel][0]
     const url = download.getAssetUrl(name, platform, arch, variant)
 
     if (url) return res.status(200).json({ url, name })
   }
 
-  res.status(204).send('No Content')
+  res.status(204).end()
 })
 
-const isReleases = (file) => (file === 'RELEASES')
-const isPkg = (file) => ((/tropy-.+\.nupkg/).test(file))
+
+api.get(`${CHANNEL}/win32/RELEASES`, winReleases)
+api.get(`${CHANNEL}/win32/:file`, winPackage)
+
+// Windows Legacy Routes (1.0.0)
+api.get(`${CHANNEL}/win32/:version/RELEASES`, winReleases)
+api.get(`${CHANNEL}/win32/:version/:file`, winPackage)
+
+
+function winReleases(req, res) {
+  const channel = req.params.channel || 'stable'
+  const arch = req.params.arch || 'x64'
+  const releases = RELEASES[arch][channel]
+
+  if (releases == null || releases.length === 0) {
+    return res.status(204).end()
+  }
+
+  res
+    .status(200)
+    .set('Content-Type', 'application/octet-stream')
+    .set('Content-Length', Buffer.byteLength(releases, 'utf8'))
+    .send(releases)
+}
+
+function winPackage(req, res) {
+  const channel = req.params.channel || 'stable'
+  //const arch = req.params.arch || 'x64'
+  const file = req.params.file
+  const match = file.match(NUPKG)
+
+  if (match != null) {
+    const version = match[3].replace(/(beta|dev|rc)/, '$1.')
+
+    if (VERSIONS[channel].indexOf(version) !== -1) {
+      const url = download.getAssetFolder(version)
+      if (url != null) return res.redirect(`${url}/${file}`)
+    }
+  }
+
+  res.status(404).send('Not Found')
+}
+
+const NUPKG =
+  /^tropy(-(beta|dev))?-(\d+\.\d+.\d+(-(beta|rc|dev)\d+)?)-(delta|full)\.nupkg$/
 
 module.exports = {
   api
--- test/views/update_test.js
+++ test/views/update_test.js
@@ -5,15 +5,28 @@ const versions = require('../../versions')
 const route = require('../support/route')
 
 describe('GET /update/(:channel/):platform/:version', () => {
-  it('no content for latest release', () =>
+  it('no content for latest mac release', () =>
     request(app)
       .get(`/update/stable/darwin/${versions.stable[0]}`)
       .then(res => { expect(res).to.have.status(204) }))
+  it('no content for latest linux release', () =>
+    request(app)
+      .get(`/update/stable/linux/${versions.stable[0]}`)
+      .then(res => { expect(res).to.have.status(204) }))
+  it('no content for latest linux x32 release', () =>
+    request(app)
+      .get(`/update/stable/linux/x32/${versions.stable[0]}`)
+      .then(res => { expect(res).to.have.status(204) }))
 
-  it('no content for latest beta', () =>
+  it('no content for latest mac beta', () =>
     request(app)
       .get(`/update/beta/darwin/${versions.beta[0]}`)
       .then(res => { expect(res).to.have.status(204) }))
+  it('no content for latest mac beta', () =>
+    request(app)
+      .get(`/update/beta/linux/${versions.beta[0]}`)
+      .then(res => { expect(res).to.have.status(204) }))
+
 
   describe('when an update is available', () => {
     beforeEach(() =>
@@ -34,26 +47,62 @@ describe('GET /update/(:channel/):platform/:version', () => {
   })
 })
 
-describe('GET /update/(:channel/):platform/:version/RELEASES', () => {
-  it('redirects', () =>
+
+describe('GET /update/(:channel/)win32/:version/RELEASES', () => {
+  it('is OK for 1.0.0', () =>
     request(app)
-      .get('/update/stable/win32/1.0.0-beta.13/RELEASES')
-      .redirects(0)
-      .then(route.unreachable, err =>
-        expect(err.response).to.have.status(302)))
+      .get('/update/stable/win32/1.0.0/RELEASES')
+      .then((res) => {
+        expect(res).to.have.status(200)
+      }, route.unreachable))
+  it('is OK for 1.0.0-beta.16', () =>
+    request(app)
+      .get('/update/beta/win32/1.0.0-beta.16/RELEASES')
+      .then((res) => {
+        expect(res).to.have.status(200)
+      }, route.unreachable))
 })
 
-describe('GET /update/(:channel/):platform/:version/:pkg', () => {
-  it('redirects for good package names', () =>
+
+describe('GET /update/(:channel/)win32/:version/:pkg', () => {
+  it('redirects for good package names for 1.0.0', () =>
     request(app)
-      .get('/update/stable/win32/1.0.0-rc.1/tropy-1.0.0-rc1-full.nupkg')
+      .get('/update/stable/win32/1.0.0/tropy-1.0.0-full.nupkg')
       .redirects(0)
       .then(route.unreachable, res =>
         expect(res).to.have.status(302)))
-
+  it('redirects for good package names for beta.16', () =>
+    request(app)
+      .get('/update/beta/win32/1.0.0-beta.16/tropy-beta-1.0.0-beta16-delta.nupkg')
+      .redirects(0)
+      .then(route.unreachable, res =>
+        expect(res).to.have.status(302)))
   it('not found for bad package names', () =>
     request(app)
       .get('/update/stable/win32/1.0.0-rc.1/tropy-beta-1.0.0-rc1-full.dmg')
+      .then(route.unreachable, res =>
+        expect(res).to.have.status(404)))
+})
+
+describe('GET /update/(:channel/)win32/RELEASES', () => {
+  it('is OK', () =>
+    request(app)
+      .get('/update/stable/win32/RELEASES')
+      .then((res) => {
+        expect(res).to.have.status(200)
+      }, route.unreachable))
+})
+
+describe('GET /update/(:channel/)win32/:pkg', () => {
+  it('redirects for good package names', () =>
+    request(app)
+      .get('/update/stable/win32/tropy-1.0.0-full.nupkg')
+      .redirects(0)
+      .then(route.unreachable, res =>
+        expect(res).to.have.status(302)))
+  it('not found for bad package names', () =>
+    request(app)
+      .get('/update/stable/win32/tropy-beta-1.0.0-rc1-full.dmg')
       .then(route.unreachable, res =>
         expect(res).to.have.status(404)))
 })
