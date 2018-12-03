15c731a59a53faef752ceb4560be89590c5f3341
Sylvester Keil <sylvester@keil.or.at>
2018-12-03 20:58:20 +0100

    Add jobs validation test

---
+++ test/views/jobs_test.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const app = require('../../app')
+const validate = require('html-validator')
+
+describe('GET /', () => {
+
+  it('is OK', () =>
+    request(app).get('/jobs')
+      .then(res => { expect(res).to.have.status(200) }))
+
+  it('is valid', function (done) {
+    this.timeout(10000)
+
+    request(app).get('/jobs')
+      .then(({ text: data })=> {
+        expect(data).to.have.string('</html>')
+
+        validate({ data }, (err, res) => {
+          expect(err).not.to.exist
+
+          expect(JSON.parse(res))
+            .to.have.property('messages')
+            .and.eql([])
+
+          done()
+        })
+      }, done)
+  })
+
+})
